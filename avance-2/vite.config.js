23d1b1082b25b08cc08a609491b3a15fe29d2a57
KevinSplits <130183913+KevinSplits@users.noreply.github.com>
2024-10-03 23:05:35 -0500

    Initial commit

--- avance-2/vite.config.js
+++ avance-2/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Avance-2--Marcos-de-desarrollo-Web/",
+  build: {
+    chunkSizeWarningLimit: 1000, // por ejemplo, ajusta a 1000 kB
+  },
 })
+
