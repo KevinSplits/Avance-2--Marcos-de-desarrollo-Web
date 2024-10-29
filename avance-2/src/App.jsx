26c61c6e8d164e867c13d6b908efa190dce08fa4
Gustavo Inga <gustavoa_it@hotmail.com>
2024-10-29 17:45:54 -0500

    cambie en App el Demo por SignIn

--- avance-2/src/App.jsx
+++ avance-2/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 import Demo from './Components/Demo'
+import SignIn from './Components/SignIng'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Demo />
+      < SignIn />
     </>
   )
 }
